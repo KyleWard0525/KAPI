9c940fa0d79719b57e106867702e23ba4b7172c6
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-08-27 15:53:44 -0400

    Main API file

---
+++ src/KWARD_API.cpp
@@ -0,0 +1,22 @@
+/**
+ * Main API file
+ * 
+ * kward
+ */
+
+#include "../include/KWARD_API.h"
+
+void KWARD_API::init()
+{
+    ; // Do nothing
+}
+
+KWARD_API::KWARD_API()
+{
+    init();
+}
+
+void KWARD_API::test()
+{
+    printf("\n\nTest.\n\n");
+}
