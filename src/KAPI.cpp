805346ba366fa4df4535c8098f1f8c875e619894
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-10-21 11:34:20 -0400

    Refactored to new API name

---
+++ src/KAPI.cpp
@@ -0,0 +1,22 @@
+/**
+ * Main API file
+ * 
+ * kward
+ */
+
+#include "../include/KAPI.h"
+
+void KAPI::init()
+{
+    ; // Do nothing
+}
+
+KAPI::KAPI()
+{
+    init();
+}
+
+void KAPI::test()
+{
+    printf("\n\nTest.\n\n");
+}
