10c52fb9a450dcd26684c55cf146e76ba5062e7f
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-11-30 14:47:29 -0500

    K API

---
+++ include/KAPI.h
@@ -0,0 +1,48 @@
+#ifndef KAPI_H
+#define KAPI_H
+
+/**
+ * Main API file
+ * 
+ * kward
+ */
+
+#include "kLib.h"
+#include "kRandom.h"
+#include "kTypes.h"
+#include "kUtils.h"
+
+//  Ensure cross-compatibility with C
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+//  Define DLL build command and type
+#ifdef BUILD_DLL
+    #define K_API __declspec(dllexport)
+#else
+    #define K_API __declspec(dllimport)
+#endif
+
+
+// Main API class
+class K_API KAPI {
+
+    private:
+
+        void init();
+
+    public:
+
+        KAPI();
+
+        void test();
+
+};
+
+
+#ifdef __cplusplus
+    }
+#endif
+
+#endif // End KAPI_H
