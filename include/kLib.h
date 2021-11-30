7c964ac9a817e731123e8ebbbd1160de4353b731
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-11-30 14:47:44 -0500

    Project library header

---
+++ include/kLib.h
@@ -0,0 +1,32 @@
+#ifndef K_LIB_H
+#define K_LIB_H
+
+/**
+ * Project library
+ * 
+ * kward
+ */
+
+//  Include in DLL build
+#ifdef BUILD_DLL
+    #define K_API __declspec(dllexport)
+#else
+    #define K_API __declspec(dllimport)
+#endif
+
+#include <iostream>                 //  cout
+#include <vector>                   //  vectors
+#include <stdio.h>                  //  printf and fopen
+#include <fstream>                  //  File IO
+#include <chrono>                   //  Timing
+#include <iomanip>                  //  IO manipulation (ex. setprecision)
+#include <sys/stat.h>               //  OS information gathering
+#include <map>                      //  For mapping data
+#include <string>                   //  Strings
+#include <string.h>                 //  Also strings
+#include <math.h>                   //  pow()
+#include <algorithm>                //  important algos
+#include <omp.h>                    //  OpenMP for multi-processing
+
+
+#endif // End K_LIB_H
